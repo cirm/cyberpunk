becfe37841deac759678cee8676dcfdc4329e76e
Madis <m.jullinen@adcash.com>
2017-06-30 10:58:15 +0300

    done

--- server/src/models/gridModel.js
+++ server/src/models/gridModel.js
@@ -1,6 +1,8 @@
 // @flow
 
-const cloneDeep = require('lodash/fp/cloneDeep');
+const _cloneDeep = require('lodash/fp/cloneDeep');
+const _reduce = require('lodash/fp/reduce');
+const _range = require('lodash/fp/range');
 const _ = require('lodash');
 const fd = require('lodash/fp');
 const cellModel = require('./cellModel');
@@ -13,26 +15,26 @@ let gameGrid: Grid = {
 };
 
 const functionalConnect = (baseGrid: Grid): Grid =>
-  _.reduce(baseGrid.cells, (result: Grid, value: Cell, key: number): Grid => {
+  _reduce((result: Grid, value: Cell): Grid => {
     // connect up
-    if (key >= baseGrid.side) {
-      value.connected.up = key - baseGrid.side;
+    if (value.pos >= baseGrid.side) {
+      value.connected.up = value.pos - baseGrid.side;
     }
     // connect down
-    if (key < baseGrid.cells.length - baseGrid.side) {
-      value.connected.down = key + baseGrid.side;
+    if (value.pos < baseGrid.cells.length - baseGrid.side) {
+      value.connected.down = value.pos + baseGrid.side;
     }
     // connect right
-    if (((key + 1) % baseGrid.side) !== 0) {
-      value.connected.right = key + 1;
+    if (((value.pos + 1) % baseGrid.side) !== 0) {
+      value.connected.right = value.pos + 1;
     }
     // connect left
-    if (key % baseGrid.side !== 0) {
-      value.connected.left = key - 1;
+    if (value.pos % baseGrid.side !== 0) {
+      value.connected.left = value.pos - 1;
     }
     result.cells.push(value);
     return result;
-  }, { side: baseGrid.side, cells: [] });
+  }, { side: baseGrid.side, cells: [] })(baseGrid.cells);
 
 const updateGridHistory = (grid: Grid): Grid => {
   gridHistory.push(grid);
@@ -42,17 +44,17 @@ const updateGridHistory = (grid: Grid): Grid => {
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 
 const buildGrid = (raw: Grid): Grid => {
-  const grid = { side: raw.side, cells: raw.cells };
+  const grid: Grid = { side: raw.side, cells: raw.cells };
   if (raw.cells.length === 0) {
-    grid.cells = _.times(grid.side * grid.side).reduce((result, val) => {
-      result.push(cellModel.buildCell(raw.cells[val], val))
+    grid.cells = _reduce((result: Cell[], val: number): Cell[] => {
+      result.push(cellModel.buildCell(grid.cells[val], val));
       return result;
-    }, []);
+    }, [])(_range(0)(grid.side * grid.side));
   } else {
-    grid.cells = _.times(grid.cells.length).reduce((result, val) => {
-      result.push(cellModel.buildCell(grid.cells[val], val))
+    grid.cells = _reduce((result: Cell[], val: number): Cell[] => {
+      result.push(cellModel.buildCell(raw.cells[val], val));
       return result;
-    }, [])
+    }, [])(_range(0)(grid.cells.length));
   }
   return grid;
 };
@@ -71,7 +73,7 @@ const createGrid = (data: ?Cell[]): Grid => {
 };
 
 const updateState = (cellPos: number, decker: string): Grid => {
-  const ngrid: Grid = cloneDeep(gameGrid);
+  const ngrid: Grid = _cloneDeep(gameGrid);
   ngrid.cells[cellPos] = cellModel.visitCell(ngrid.cells[cellPos], decker);
   gameGrid = ngrid;
   updateGridHistory(gameGrid);
